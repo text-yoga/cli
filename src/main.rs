e6387265f20ae2b56866b1759a605ecfd469c000
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2024-01-25 12:00:57 +0000

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate intel_mkl_src;
 extern crate accelerate_src;
 
 mod jina_bert;
+use crate::jina_bert::Config;
 use anyhow::Error as E;
 use candle_core::{
     utils::{cuda_is_available, metal_is_available},
@@ -12,12 +13,12 @@ use candle_core::{
 };
 use candle_nn::VarBuilder;
 use clap::Parser;
+use hf_hub::api::tokio::ApiBuilder;
+use itertools::{Either, Itertools};
 use jina_bert::BertModel;
-use tracing::{debug, info, Level};
+use tokenizers::Tokenizer;
 use tracing_chrome::ChromeLayerBuilder;
-use tracing_subscriber::{fmt, FmtSubscriber, Registry};
-
-use crate::jina_bert::Config;
+use tracing_subscriber::{fmt, Registry};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -29,14 +30,6 @@ struct Args {
     /// Enable tracing (generates a trace-timestamp.json file).
     #[arg(long)]
     tracing: bool,
-
-    /// When set, compute embeddings for this prompt.
-    #[arg(long)]
-    prompt: Option<String>,
-
-    /// The number of times to run the prompt.
-    #[arg(long, default_value = "1")]
-    n: usize,
 
     /// L2 normalization for embeddings.
     #[arg(long, default_value = "true")]
@@ -50,25 +43,33 @@ struct Args {
 }
 
 impl Args {
-    fn build_model_and_tokenizer(&self) -> anyhow::Result<(BertModel, tokenizers::Tokenizer)> {
-        use hf_hub::{api::sync::Api, Repo, RepoType};
+    async fn build_model_and_tokenizer(
+        &self,
+    ) -> anyhow::Result<(BertModel, tokenizers::Tokenizer)> {
+        use hf_hub::{Repo, RepoType};
+
+        let api = ApiBuilder::new().with_progress(true).build()?;
         let model = match &self.model {
             Some(model_file) => std::path::PathBuf::from(model_file),
-            None => Api::new()?
-                .repo(Repo::new(
+            None => {
+                api.repo(Repo::new(
                     "jinaai/jina-embeddings-v2-small-en".to_string(),
                     RepoType::Model,
                 ))
-                .get("model.safetensors")?,
+                .get("model.safetensors")
+                .await?
+            }
         };
         let tokenizer = match &self.tokenizer {
             Some(file) => std::path::PathBuf::from(file),
-            None => Api::new()?
-                .repo(Repo::new(
+            None => {
+                api.repo(Repo::new(
                     "jinaai/jina-embeddings-v2-small-en".to_string(),
                     RepoType::Model,
                 ))
-                .get("tokenizer.json")?,
+                .get("tokenizer.json")
+                .await?
+            }
         };
         let device = device(self.cpu)?;
         let mut config = Config::v2_base();
@@ -106,7 +107,68 @@ pub fn device(cpu: bool) -> anyhow::Result<Device> {
         Ok(Device::Cpu)
     }
 }
-fn main() -> anyhow::Result<()> {
+
+fn embed(
+    model: &BertModel,
+    tokenizer: &mut Tokenizer,
+    normalize_embeddings: bool,
+    sentences: Vec<String>,
+) -> Result<(Vec<(String, Vec<f32>)>, Vec<(String, candle_core::Error)>), anyhow::Error> {
+    let device = &model.device;
+    if let Some(pp) = tokenizer.get_padding_mut() {
+        pp.strategy = tokenizers::PaddingStrategy::BatchLongest
+    } else {
+        let pp = tokenizers::PaddingParams {
+            strategy: tokenizers::PaddingStrategy::BatchLongest,
+            ..Default::default()
+        };
+        tokenizer.with_padding(Some(pp));
+    }
+
+    println!("tokenizing texts");
+    let tokens = tokenizer
+        .encode_batch(sentences.clone(), true)
+        .map_err(E::msg)?;
+    let token_ids = tokens
+        .iter()
+        .map(|tokens| {
+            let tokens = tokens.get_ids().to_vec();
+            Tensor::new(tokens.as_slice(), device)
+        })
+        .collect::<candle_core::Result<Vec<_>>>()?;
+
+    let token_ids = Tensor::stack(&token_ids, 0)?;
+    println!("running inference on batch {:?}", token_ids.shape());
+    let embeddings = model.forward(&token_ids)?;
+    println!("generated embeddings {:?}", embeddings.shape());
+    // Apply some avg-pooling by taking the mean embedding value for all tokens (including padding)
+    let (_n_sentence, n_tokens, _hidden_size) = embeddings.dims3()?;
+    let embeddings = (embeddings.sum(1)? / (n_tokens as f64))?;
+    let embeddings = if normalize_embeddings {
+        normalize_l2(&embeddings)?
+    } else {
+        embeddings
+    };
+    println!("pooled embeddings {:?}", embeddings.shape());
+
+    let result: (Vec<(String, Vec<f32>)>, Vec<(String, candle_core::Error)>) = sentences
+        .into_iter()
+        .map(|s| s.clone())
+        .enumerate()
+        .partition_map(|(i, sentence)| {
+            match embeddings
+                .get(i)
+                .and_then(|embedding| embedding.to_vec1::<f32>())
+            {
+                Ok(embedding) => Either::Left((sentence, embedding)),
+                Err(err) => Either::Right((sentence, err)),
+            }
+        });
+    Ok(result)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     // use tracing_chrome::ChromeLayerBuilder;
     use tracing_subscriber::prelude::*;
 
@@ -132,95 +194,23 @@ fn main() -> anyhow::Result<()> {
     };
     let start = std::time::Instant::now();
 
-    let (model, mut tokenizer) = args.build_model_and_tokenizer()?;
-    let device = &model.device;
+    let (model, mut tokenizer) = args.build_model_and_tokenizer().await?;
 
-    if let Some(prompt) = args.prompt {
-        let tokenizer = tokenizer
-            .with_padding(None)
-            .with_truncation(None)
-            .map_err(E::msg)?;
-        let tokens = tokenizer
-            .encode(prompt, true)
-            .map_err(E::msg)?
-            .get_ids()
-            .to_vec();
-        let token_ids = Tensor::new(&tokens[..], device)?.unsqueeze(0)?;
-        println!("Loaded and encoded {:?}", start.elapsed());
-        for idx in 0..args.n {
-            let start = std::time::Instant::now();
-            let ys = model.forward(&token_ids)?;
-            if idx == 0 {
-                println!("{ys}");
-            }
-            println!("Took {:?}", start.elapsed());
-        }
-    } else {
-        let sentences = [
-            "The cat sits outside",
-            "A man is playing guitar",
-            "I love pasta",
-            "The new movie is awesome",
-            "The cat plays in the garden",
-            "A woman watches TV",
-            "The new movie is so great",
-            "Do you like pizza?",
-        ];
-        let n_sentences = sentences.len();
-        if let Some(pp) = tokenizer.get_padding_mut() {
-            pp.strategy = tokenizers::PaddingStrategy::BatchLongest
-        } else {
-            let pp = tokenizers::PaddingParams {
-                strategy: tokenizers::PaddingStrategy::BatchLongest,
-                ..Default::default()
-            };
-            tokenizer.with_padding(Some(pp));
-        }
+    let sentences = [
+        "The cat sits outside",
+        "A man is playing guitar",
+        "I love pasta",
+        "The new movie is awesome",
+        "The cat plays in the garden",
+        "A woman watches TV",
+        "The new movie is so great",
+        "Do you like pizza?",
+    ]
+    .map(String::from)
+    .to_vec();
 
-        println!("tokenizing texts");
-        let tokens = tokenizer
-            .encode_batch(sentences.to_vec(), true)
-            .map_err(E::msg)?;
-        let token_ids = tokens
-            .iter()
-            .map(|tokens| {
-                let tokens = tokens.get_ids().to_vec();
-                Tensor::new(tokens.as_slice(), device)
-            })
-            .collect::<candle_core::Result<Vec<_>>>()?;
-
-        let token_ids = Tensor::stack(&token_ids, 0)?;
-        println!("running inference on batch {:?}", token_ids.shape());
-        let embeddings = model.forward(&token_ids)?;
-        println!("generated embeddings {:?}", embeddings.shape());
-        // Apply some avg-pooling by taking the mean embedding value for all tokens (including padding)
-        let (_n_sentence, n_tokens, _hidden_size) = embeddings.dims3()?;
-        let embeddings = (embeddings.sum(1)? / (n_tokens as f64))?;
-        let embeddings = if args.normalize_embeddings {
-            normalize_l2(&embeddings)?
-        } else {
-            embeddings
-        };
-        println!("pooled embeddings {:?}", embeddings.shape());
-
-        let mut similarities = vec![];
-        for i in 0..n_sentences {
-            let e_i = embeddings.get(i)?;
-            for j in (i + 1)..n_sentences {
-                let e_j = embeddings.get(j)?;
-                let sum_ij = (&e_i * &e_j)?.sum_all()?.to_scalar::<f32>()?;
-                let sum_i2 = (&e_i * &e_i)?.sum_all()?.to_scalar::<f32>()?;
-                let sum_j2 = (&e_j * &e_j)?.sum_all()?.to_scalar::<f32>()?;
-                let cosine_similarity = sum_ij / (sum_i2 * sum_j2).sqrt();
-                similarities.push((cosine_similarity, i, j))
-            }
-        }
-        similarities.sort_by(|u, v| v.0.total_cmp(&u.0));
-        for &(score, i, j) in similarities[..5].iter() {
-            println!("score: {score:.2} '{}' '{}'", sentences[i], sentences[j])
-        }
-    }
-
+    let result = embed(&model, &mut tokenizer, false, sentences);
+    println!("Result: {:#?}", result);
     Ok(())
 }
 
